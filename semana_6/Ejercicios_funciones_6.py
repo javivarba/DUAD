bccc417ee5b18392c34253d4af5eacdca033ffa7
Javier Vargas Basulto <javivarba@gmail.com>
2025-04-10 15:18:53 -0600

    Eliminadas carpetas innecesarias del commit

--- semana_6/Ejercicios_funciones_6.py
+++
@@ -1,9 +0,0 @@
-
-def words_sequence (text):
-    list_words = text.split("-")
-    list_sequence = sorted(list_words)
-    return "-".join(list_sequence)
-
-results = words_sequence (" tiger - car - web - ball - cup - trail - apple - biscuit")
-
-print(results)
