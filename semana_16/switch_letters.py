e72c650663d6066cec177f2d0c8774359726dc21
Javier Vargas <javivarba@gmail.com>
2025-06-26 16:58:03 -0600

    Ejercicios Unit Testing semana 16

---
+++ semana_16/switch_letters.py
@@ -0,0 +1,7 @@
+def switch_letters(phrase):
+    return phrase[::-1]
+
+# Example
+my_phrase = "Pura Vida"
+result = switch_letters(my_phrase)
+print(result)  # adiV aruP
---
+++ semana_16/words_sequence.py
@@ -0,0 +1,8 @@
+def words_sequence(text):
+    list_words = text.split("-")
+    list_sequence = sorted(list_words)
+    return "-".join(list_sequence)
+
+# Ejemplo
+results = words_sequence("tiger-car-web-ball-cup-trail-apple-biscuit")
+print(results)
